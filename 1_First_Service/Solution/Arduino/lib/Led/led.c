b13dfa6c2b00884aaebd666945446115d27d6d4b
Nicolas Rabault <nicolas.rabault@luos.io>
2022-01-27 13:23:37 +0100

    Make the solution fit the tutorial

--- 1_First_Service/Solution/Arduino/lib/Led/led.c
+++ 1_First_Service/Solution/Arduino/lib/Led/led.c
@@ -1,19 +1,6 @@
 #include "led.h"
 #include "Arduino.h"
 
-void Led_MsgHandler(service_t *service, msg_t *msg);
-
-void Led_Init(void)
-{
-
-    revision_t revision = {.major = 1, .minor = 0, .build = 0};
-    Luos_CreateService(Led_MsgHandler, STATE_TYPE, "led", revision);
-
-    pinMode(LED_BUILTIN, OUTPUT);
-}
-
-void Led_Loop(void) {}
-
 void Led_MsgHandler(service_t *service, msg_t *msg)
 {
     if (msg->header.cmd == IO_STATE)
@@ -28,3 +15,14 @@ void Led_MsgHandler(service_t *service, msg_t *msg)
         }
     }
 }
+
+void Led_Init(void)
+{
+
+    revision_t revision = {1, 0, 0};
+    Luos_CreateService(Led_MsgHandler, STATE_TYPE, "led", revision);
+
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void Led_Loop(void) {}
