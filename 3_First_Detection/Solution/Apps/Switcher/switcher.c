f6a1ee13790d8477e1483dd4cf98fe5a6def9bd3
Salem_Luos <thomas.salembier@luos.io>
2022-02-01 10:44:48 +0100

    linting and generic app

--- 3_First_Detection/Solution/Apps/Switcher/switcher.c
+++ 3_First_Detection/Solution/Apps/Switcher/switcher.c
@@ -1,4 +1,3 @@
-#include <Arduino.h>
 #include "switcher.h"
 
 /*******************************************************************************
@@ -6,7 +5,7 @@
  ******************************************************************************/
 typedef enum
 {
-    SWITCHER_APP = LUOS_LAST_TYPE
+  SWITCHER_APP = LUOS_LAST_TYPE
 } App_type_t;
 
 /*******************************************************************************
@@ -23,51 +22,53 @@ uint32_t ActualTime;
  ******************************************************************************/
 void Switcher_MsgHandler(service_t *service, msg_t *msg)
 {
-    if (msg->header.cmd == END_DETECTION)
+  if (msg->header.cmd == END_DETECTION)
+  {
+    ID_Led = RoutingTB_IDFromAlias("led");
+    ID_Button = RoutingTB_IDFromAlias("button");
+  }
+  else if ((msg->header.cmd == IO_STATE) &&
+           (msg->header.source == ID_Button))
+  {
+    if (ID_Led != 0)
     {
-        ID_Led = RoutingTB_IDFromAlias("led");
-        ID_Button = RoutingTB_IDFromAlias("button");
+      msg_t pub_msg;
+      pub_msg.header.cmd = IO_STATE;
+      pub_msg.header.target_mode = ID;
+      pub_msg.header.target = ID_Led;
+      pub_msg.header.size = 1;
+      pub_msg.data[0] = msg->data[0];
+      Luos_SendMsg(switcher_app, &pub_msg);
     }
-    else if ((msg->header.cmd == IO_STATE) && (msg->header.source == ID_Button))
-    {
-        if (ID_Led != 0)
-        {
-            msg_t pub_msg;
-            pub_msg.header.cmd = IO_STATE;
-            pub_msg.header.target_mode = ID;
-            pub_msg.header.target = ID_Led;
-            pub_msg.header.size = 1;
-            pub_msg.data[0] = msg->data[0];
-            Luos_SendMsg(switcher_app, &pub_msg);
-        }
-    }
+  }
 }
 
 void Switcher_Init(void)
 {
-    revision_t revision = {1, 0, 0};
-    switcher_app = Luos_CreateService(Switcher_MsgHandler, SWITCHER_APP, "Switcher", revision);
-    Luos_Detect(switcher_app);
-    ActualTime = LuosHAL_GetSystick();
+  revision_t revision = {1, 0, 0};
+  switcher_app = Luos_CreateService(Switcher_MsgHandler, SWITCHER_APP,
+                                    "Switcher", revision);
+  Luos_Detect(switcher_app);
+  ActualTime = LuosHAL_GetSystick();
 }
 
 void Switcher_Loop(void)
 {
-    msg_t pub_msg;
-    if (Luos_IsNodeDetected() == true) // Topology detection Finish
+  msg_t pub_msg;
+  if (Luos_IsNodeDetected() == true) // Topology detection Finish
+  {
+    // ask button value every 10ms
+    if ((Luos_GetSystick() - ActualTime) > 10)
     {
-        // ask button value every 10ms
-        if ((LuosHAL_GetSystick() - ActualTime) > 10)
-        {
-            ActualTime = LuosHAL_GetSystick();
-            if (ID_Button != 0)
-            {
-                pub_msg.header.cmd = IO_STATE;
-                pub_msg.header.target_mode = ID;
-                pub_msg.header.target = ID_Button;
-                pub_msg.header.size = 0;
-                Luos_SendMsg(switcher_app, &pub_msg);
-            }
-        }
+      ActualTime = LuosHAL_GetSystick();
+      if (ID_Button != 0)
+      {
+        pub_msg.header.cmd = IO_STATE;
+        pub_msg.header.target_mode = ID;
+        pub_msg.header.target = ID_Button;
+        pub_msg.header.size = 0;
+        Luos_SendMsg(switcher_app, &pub_msg);
+      }
     }
+  }
 }
