97cee5d52a7d2ad030d2d1408d5e3f48a4fa753a
Nicolas Rabault <nicolas.rabault@luos.io>
2022-05-13 13:37:14 +0200

    Linting

--- 2_First_Message/Solution/Arduino/lib/Button/button.c
+++ 2_First_Message/Solution/Arduino/lib/Button/button.c
@@ -16,7 +16,7 @@ service_t *button_service;
 void Button_Init(void)
 {
     revision_t revision = {1, 0, 0};
-    button_service = Luos_CreateService(0, STATE_TYPE, "button", revision);
+    button_service      = Luos_CreateService(0, STATE_TYPE, "button", revision);
     pinMode(BTN_PIN, INPUT);
 }
 
@@ -29,11 +29,11 @@ void Button_Loop(void)
         {
             // fill the message infos
             msg_t pub_msg;
-            pub_msg.header.cmd = IO_STATE;
+            pub_msg.header.cmd         = IO_STATE;
             pub_msg.header.target_mode = IDACK;
-            pub_msg.header.target = msg->header.source;
-            pub_msg.header.size = sizeof(char); // 1 byte
-            pub_msg.data[0] = digitalRead(BTN_PIN);
+            pub_msg.header.target      = msg->header.source;
+            pub_msg.header.size        = sizeof(char); // 1 byte
+            pub_msg.data[0]            = digitalRead(BTN_PIN);
             Luos_SendMsg(button_service, &pub_msg);
         }
     }
--- 2_First_Message/Solution/NUCLEO-L432KC/lib/Button/button.c
+++ 2_First_Message/Solution/NUCLEO-L432KC/lib/Button/button.c
@@ -16,7 +16,7 @@ service_t *button_service;
 void Button_Init(void)
 {
     revision_t revision = {1, 0, 0};
-    button_service = Luos_CreateService(0, STATE_TYPE, "button", revision);
+    button_service      = Luos_CreateService(0, STATE_TYPE, "button", revision);
 }
 
 void Button_Loop(void)
@@ -28,11 +28,11 @@ void Button_Loop(void)
         {
             // fill the message infos
             msg_t pub_msg;
-            pub_msg.header.cmd = IO_STATE;
+            pub_msg.header.cmd         = IO_STATE;
             pub_msg.header.target_mode = IDACK;
-            pub_msg.header.target = msg->header.source;
-            pub_msg.header.size = sizeof(char); // 1 byte
-            pub_msg.data[0] = HAL_GPIO_ReadPin(BTN_GPIO_Port, BTN_Pin);
+            pub_msg.header.target      = msg->header.source;
+            pub_msg.header.size        = sizeof(char); // 1 byte
+            pub_msg.data[0]            = HAL_GPIO_ReadPin(BTN_GPIO_Port, BTN_Pin);
             Luos_SendMsg(button_service, &pub_msg);
         }
     }
